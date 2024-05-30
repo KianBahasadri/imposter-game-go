4ba60023a2f2fedc9b592a3fd1025420ce766d79
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2024-05-30 23:07:59 +0000

    added stripe integration

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.22.2
 
 require (
 	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/stripe/stripe-go/v78 v78.9.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,19 @@ import (
   "net/http"
   "encoding/json"
   "sort"
+  "github.com/stripe/stripe-go/v78"
+  "github.com/stripe/stripe-go/v78/checkout/session"
+  "fmt"
+  "log"
+  "os"
 )
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
   http.ServeFile(w, r, "templates/login.html")
+}
+
+func serveLogo (w http.ResponseWriter, r *http.Request) {
+  http.ServeFile(w, r, "logo.webp")
 }
 
 func queryRooms(w http.ResponseWriter, r *http.Request) {
@@ -34,14 +43,46 @@ func websocketRouter(w http.ResponseWriter, r *http.Request) {
   serveWs(activeHubs[roomName], w, r)
 }
   
+func createCheckoutSession(w http.ResponseWriter, r *http.Request) {
+  domain := "http://localhost:4242"
+  params := &stripe.CheckoutSessionParams{
+    LineItems: []*stripe.CheckoutSessionLineItemParams{
+      &stripe.CheckoutSessionLineItemParams{
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        Price: stripe.String("price_1PMI0ZAM8ZGd4LYrBs3ucPtp"),
+        Quantity: stripe.Int64(1),
+      },
+    },
+    Mode: stripe.String(string(stripe.CheckoutSessionModePayment)),
+    SuccessURL: stripe.String(domain + "/success.html"),
+    CancelURL: stripe.String(domain + "/cancel.html"),
+  }
+  s, err := session.New(params)
+  if err != nil {
+    fmt.Printf("session.New: %v", err)
+  }
+  http.Redirect(w, r, s.URL, http.StatusSeeOther)
+}
+
+
 var activeHubs = make(map[string]*Hub)
 
 func main() {
+  apikey, err := os.ReadFile("keys/stripe.key")
+  if err != nil {
+    fmt.Printf("Failed to read API key: %v", err)
+  }
+  stripe.Key = string(apikey[:len(apikey)-1])
   http.HandleFunc("/{$}", loginHandler) // only matches "/"
+  http.HandleFunc("/logo.webp", serveLogo)
   http.HandleFunc("/queryRooms", queryRooms)
   http.HandleFunc("POST /createHub", createHub)
   http.HandleFunc("/game", gameHandler)
   http.HandleFunc("/ws/{roomName}", websocketRouter)
+  http.HandleFunc("/create-checkout-session", createCheckoutSession)
 
-  http.ListenAndServe(":8080", nil)
+ log.Fatal(http.ListenAndServeTLS(":443", "keys/imposter.bahasadri.com.pem", "keys/imposter.bahasadri.com.key", nil))
 }
+
+
+
